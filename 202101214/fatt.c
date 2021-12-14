0e22797084da67ae43fac333484d638eda36edd5
alfredogiacconi <94383073+alfredogiacconi@users.noreply.github.com>
2021-12-14 15:19:03 +0100

    Update fatt.c

--- 202101214/fatt.c
+++ 202101214/fatt.c
@@ -1,10 +1,10 @@
 #include "fatt.h"
 
-int fatt(int n) {
-  if(n==1) {
+int long fatt(long int n) {
+  if(n==1L) {
     return 1;
   }
   else {
-    return (n * fatt(n-1));
+    return (n * fatt(n-1L));
   }
 }
